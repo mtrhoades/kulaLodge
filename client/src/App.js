235ac34c718f70541e53f8a374aefa634b88633e
root <mtrhoades0727@gmail.com>
2022-12-05 22:14:51 -1000

    starting dashboard now, got login button to work as a route for now

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,71 @@
 import './App.css';
-import Login from './components/Login'
+import { Link } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <div style={{display: 'flex', justifyContent: 'center', alignItems: 'center'}}>
-        <Login />
+
+    return (
+      <div>
+        
+      <h1>Welcome</h1>
+
+      <form action='' method='get'>
+          <div className='username' style={{paddingBottom: '10px'}}>
+              <label
+                  style={{paddingRight: '10px'}}
+                  for='user'
+              >
+                  Username:
+              </label>
+
+              <input
+                  class='input user'
+                  type='text'
+                  placeholder='Enter Username'
+                  // required
+                  value=''
+              >
+              </input>
+          </div>
+
+          <div className='password'>
+              <label
+                  style={{paddingRight: '10px'}}
+                  for='password'
+              >
+                  Password: 
+              </label>
+
+              <input
+                  class='input pw'
+                  type='text'
+                  placeholder='Enter Password'
+                  // required
+                  value=''
+              >
+              </input>
+          </div>
+              
+          {/* submit button here, and for now take it to the dashboard page until we get login and sign up working with authentication and authorization */}
+      </form>
+
+      <div className='submitButton'>
+
+
+          <Link
+            to='/Dashboard'
+          >
+
+            <button 
+              id='loginButton' 
+              type='button'
+            >
+              Login
+            </button>
+
+          </Link>
       </div>
-    </div>
+
+  </div>
   );
 }
 
--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Dashboard from './components/Dashboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<App />} /> 
+        <Route path='/Dashboard' element={<Dashboard />} /> 
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>
 );
+
+
 
